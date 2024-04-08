f697f55927d21a8ab5b9fab9910d383b6a83e153
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-04-08 19:16:21 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+from my_module import hey_world as hew, hello_world as hw
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    hew()
+    hw()
+    hw()
+    hew()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ my_module.py
@@ -0,0 +1,7 @@
+def hey_world():
+    """print Hey World"""
+    print("Hey World!")
+
+def hello_world():
+    """print Hello World!"""
+    print("Hello World!")
